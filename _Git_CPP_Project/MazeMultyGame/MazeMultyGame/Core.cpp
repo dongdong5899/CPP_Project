d9a8c201379bd00e863a3eaaefa894ba77867e90
zhdltl601 <zhdltl601@gmail.com>
2024-06-11 20:03:43 +0900

    ihatevcxprojfilters.....

--- _Git_CPP_Project/MazeMultyGame/MazeMultyGame/Core.cpp
+++ _Git_CPP_Project/MazeMultyGame/MazeMultyGame/Core.cpp
@@ -1,22 +1,31 @@
 #include "Core.h"
 #include "MazeMap.h"
 #include <string.h>
-Core* Core::m_pInst = nullptr;
-Core::Core(){
+#include <algorithm>
+#include "Core.h"
+#include "Console.h"
+	Core * Core::m_pInst = nullptr;
+Core::Core() {
 
 }
-bool Core::Init(Player* _player)
+bool Core::Init(Player* _player, Player* _player2)
 {
-	strcpy_s(arrMap[0],  "21111100000000000000");
-	strcpy_s(arrMap[1],  "00000111111110000000");
-	strcpy_s(arrMap[2],  "00111100000000000000");
-	strcpy_s(arrMap[3],  "00000100000000000000");
-	strcpy_s(arrMap[4],  "00000111111111100000");
-	strcpy_s(arrMap[5],  "00000100000000100000");
-	strcpy_s(arrMap[6],  "00000100000000100000");
-	strcpy_s(arrMap[7],  "00000100000000100000");
-	strcpy_s(arrMap[8],  "00000100000000100000");
-	strcpy_s(arrMap[9],  "00000100000000100000");
+	system("title 22Bombman | mode con cols=80 lines=40");
+
+	CursorVis(false, 1);
+	LockResize();
+	SetFontSize(FW_BOLD, 20, 20);
+
+	strcpy_s(arrMap[0], "11111100000000000000");
+	strcpy_s(arrMap[1], "00000111111110000000");
+	strcpy_s(arrMap[2], "00111100000000000000");
+	strcpy_s(arrMap[3], "00000100000000000000");
+	strcpy_s(arrMap[4], "00000111111111100000");
+	strcpy_s(arrMap[5], "00000100000000100000");
+	strcpy_s(arrMap[6], "00000100000000100000");
+	strcpy_s(arrMap[7], "00000100000000100000");
+	strcpy_s(arrMap[8], "00000100000000100000");
+	strcpy_s(arrMap[9], "00000100000000100000");
 	strcpy_s(arrMap[10], "00000100000000111100");
 	strcpy_s(arrMap[11], "00000100000000000000");
 	strcpy_s(arrMap[12], "00000111111111111100");
@@ -27,8 +36,8 @@ bool Core::Init(Player* _player)
 	strcpy_s(arrMap[17], "00000100000000000100");
 	strcpy_s(arrMap[18], "00000111111111111100");
 	strcpy_s(arrMap[19], "00000000000000000000");
-	//arrMap = arrMap;
 	player = _player;
+	player2 = _player2;
 	return true;
 }
 void Core::Run()
@@ -46,15 +55,41 @@ void Core::Run()
 }
 void Core::Update()
 {
-
+	Vector2 newPos = player->vector;
 	if (GetAsyncKeyState(VK_UP) & 0x8000)
-		player->vector->y--;
+		newPos.y--;
 	if (GetAsyncKeyState(VK_DOWN) & 0x8000)
-		player->vector->y++;
+		newPos.y++;
 	if (GetAsyncKeyState(VK_LEFT) & 0x8000)
-		player->vector->x--;
+		newPos.x--;
 	if (GetAsyncKeyState(VK_RIGHT) & 0x8000)
-		player->vector->x++;
+		newPos.x++;
+
+	//clamp
+	newPos.x = newPos.x < MAP_WIDTH - 2 ? newPos.x : MAP_WIDTH - 2;
+	newPos.y = newPos.y < MAP_WIDTH - 1 ? newPos.y : MAP_HEIGHT - 1;
+
+	newPos.x = newPos.x > 0 ? newPos.x : 0;
+	newPos.y = newPos.y > 0 ? newPos.y : 0;
+	player->vector = newPos;
+
+	Vector2 newPos2 = player2->vector;
+	if (GetAsyncKeyState(VK_UP) & 0x8000)
+		newPos2.y--;
+	if (GetAsyncKeyState(VK_DOWN) & 0x8000)
+		newPos2.y++;
+	if (GetAsyncKeyState(VK_LEFT) & 0x8000)
+		newPos2.x--;
+	if (GetAsyncKeyState(VK_RIGHT) & 0x8000)
+		newPos2.x++;
+
+	//clamp
+	newPos2.x = newPos2.x < MAP_WIDTH - 2 ? newPos2.x : MAP_WIDTH - 2;
+	newPos2.y = newPos2.y < MAP_WIDTH - 1 ? newPos2.y : MAP_HEIGHT - 1;
+
+	newPos2.x = newPos2.x > 0 ? newPos2.x : 0;
+	newPos2.y = newPos2.y > 0 ? newPos2.y : 0;
+	player2->vector = newPos2;
 }
 
 void Core::Render()
@@ -63,18 +98,22 @@ void Core::Render()
 	{
 		for (int j = 0; j < MAP_WIDTH; ++j)
 		{
-			if(player->vector->x == j && player->vector->y == i)
-				cout << "P ";
-			else if (arrMap[i][j] == (char)OBJ_TYPE::WALL)
-				cout << "¡á";
+			if (arrMap[i][j] == (char)OBJ_TYPE::WALL)
+				cout << "¤±";
 			else if (arrMap[i][j] == (char)OBJ_TYPE::ROAD)
 				cout << "  ";
 			else if (arrMap[i][j] == (char)OBJ_TYPE::START)
-				cout << "¡Ú";
+				cout << "¤»";
 			else if (arrMap[i][j] == (char)OBJ_TYPE::GOAL)
-				cout << "¢Í";
-			
+				cout << "¤º";
 		}
 		cout << endl;
 	}
+	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD Cur = { player->vector.x * 2, player->vector.y };
+	SetConsoleCursorPosition(hOut, Cur);
+	cout << "³ë";
+
+	//HANDLE hOut2 = GetStdHandle(STD_OUTPUT_HANDLE);
+	//cout << "¹«";
 }
--- _Git_CPP_Project/MazeMultyGame/MazeMultyGame/Main.cpp
+++ _Git_CPP_Project/MazeMultyGame/MazeMultyGame/Main.cpp
@@ -5,11 +5,8 @@ int main() {
 	Core* c = Core::GetInst();
 	Player* p = new Player();
 	Player* p2 = new Player();
-	p->vector = Vector2(1, 0);
+	p->vector = Vector2(0, 0);
 	p2->vector = Vector2(0, 0);
-	//p.vector = new Vector2(1,0);
-	//p2->vector = new Vector2(3,0);
-	//Vector2 a = Vector2::up;
 
 	c->Init(p, p2);
 	c->Run();
