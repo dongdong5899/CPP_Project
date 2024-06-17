af62a813a2e571bf0af425961baf4a86027d2aed
dongdong5899 <dong012673@gmail.com>
2024-06-17 14:01:11 +0900

    .

--- _Git_CPP_Project/MazeMultyGame/MazeMultyGame/Main.cpp
+++ _Git_CPP_Project/MazeMultyGame/MazeMultyGame/Main.cpp
@@ -6,7 +6,7 @@ int main() {
 	Player* p = new Player(false);
 	Player* p2 = new Player(true);
 	p->currentPos = Vector2(0, 0);
-	p2->currentPos = Vector2(0, 0);
+	p2->currentPos = Vector2(MAP_WIDTH - 1, MAP_HEIGHT - 1);
 
 	c->Init(p, p2);
 	c->Run();
