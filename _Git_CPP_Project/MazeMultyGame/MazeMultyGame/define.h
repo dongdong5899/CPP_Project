ef85007ddded26c4813e7623ebf31cf5df14f8eb
dongdong5899 <dong012673@gmail.com>
2024-06-24 15:24:02 +0900

    Item

--- _Git_CPP_Project/MazeMultyGame/MazeMultyGame/define.h
+++ _Git_CPP_Project/MazeMultyGame/MazeMultyGame/define.h
@@ -3,8 +3,8 @@
 using std::cout;
 using std::wcout;
 using std::endl;
-const int MAP_WIDTH = 21;
-const int MAP_HEIGHT = 20;
+const int MAP_WIDTH = 41;
+const int MAP_HEIGHT = 40;
 #include<Windows.h>
 #define SAFE_DELETE(p) if (p != nullptr){ delete p;	p = nullptr;}
 #define RenderLevel 7
