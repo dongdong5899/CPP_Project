b32ad5c45d8d126dc28107f34185d7828aa79d49
zhdltl601 <zhdltl601@gmail.com>
2024-06-29 13:25:59 +0900

    .

--- _Git_CPP_Project/MazeMultyGame/MazeMultyGame/Item_A_RandomMove.cpp
+++ _Git_CPP_Project/MazeMultyGame/MazeMultyGame/Item_A_RandomMove.cpp
@@ -5,11 +5,11 @@
 #include <time.h>
 void Item_A_RandomMove::UseItem(Player* player)
 {
-	//random move
-	Vector2 pos = Log::GetPos();
-	GotoPos(pos.x, pos.y);
-	cout << "used_item";
-	cout << "_random_Move";//item name
+	//debug
+	//Vector2 pos = Log::GetPos();
+	//GotoPos(pos.x, pos.y);
+	//cout << "used_item";
+	//cout << "_random_Move";//item name
 	Vector2 randomPos = getRandomPos(player);
 	player->newPos = randomPos;
 }
